6d53afe5606f5bfda72d7304f0f2e2483f938b32
Alcatraz <markin201045@gmail.com>
2019-04-01 12:57:50 +0800

    daily upload

--- src/algor_test.cpp
+++ src/algor_test.cpp
@@ -3,33 +3,107 @@
 #include "math.h"
 #include <time.h>
 #include <cmath>
+#include <ros/ros.h>
+#include <multiple_rb_ctrl/occupy_grid_srv.h>
 
 using namespace std;
 
-float round_coor(float num_to_round)
+#define occupy true
+#define disoccupy false
+
+class Occupied_grid_map
 {
-  float result = 0;
-  int int_part = (int)num_to_round;
-  float little_part = num_to_round - int_part;
-  float sign = num_to_round / abs(num_to_round);
-  if (abs(little_part) >= 0.25 && abs(little_part) < 0.75)
+private:
+  // occupied:true  not occupied:false
+  bool _occupied[19][19];
+  uint8_t _occupied_by_who[19][19];
+
+public:
+  bool check_if_occupied(uint32_t point[2])
   {
-    result = sign * (abs((float)int_part) + 0.5);
+    if (_occupied[point[0]][point[1]])
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
   }
-  else if (abs(little_part) < 0.25)
+  uint8_t check_occupied_by_who(uint32_t point[2])
   {
-    result = sign * abs(int_part);
+    return _occupied_by_who[point[0]][point[1]];
   }
-  else if (abs(little_part) >= 0.75)
+  bool set_occupied(uint32_t point[2], uint8_t who)
   {
-    result = sign * (abs(int_part) + 1);
+    if (_occupied[point[0]][point[1]])
+    {
+      return false;
+    }
+    else
+    {
+      _occupied[point[0]][point[1]] = true;
+      _occupied_by_who[point[0]][point[1]] = who;
+      return true;
+    }
   }
-  cout << sign << endl;
-  return result;
+  bool set_disoccupy(uint32_t point[2])
+  {
+    _occupied[point[0]][point[1]] = false;
+    _occupied_by_who[point[0]][point[1]] = 0;
+    return true;
+  }
+  Occupied_grid_map(void)
+  {
+    for (uint16_t i = 0; i < 19; i++)
+    {
+      for (uint16_t j = 0; j < 19; j++)
+      {
+        _occupied[i][j] = false;
+      }
+    }
+  }
+};
+
+bool server_callback(multiple_rb_ctrl::occupy_grid_srv::Request &req, multiple_rb_ctrl::occupy_grid_srv::Response &res)
+{
+  // operation 1: apply to occupy
+  // operation 2: apply to disoccupy
+  uint32_t point[2];
+  clock_t start_time, end_time;
+  static Occupied_grid_map Ocg;
+  point[0] = (uint32_t)(req.point_to_apply.y / 0.5 + 9);
+  point[1] = (uint32_t)(req.point_to_apply.x / 0.5 + 9);
+  ROS_INFO("received request");
+  start_time = clock();
+  if (req.operation == occupy)
+  {
+    if (!Ocg.check_if_occupied(point))
+    {
+      Ocg.set_occupied(point, req.applier);
+      res.succeed_or_not = true;
+    }
+    else
+    {
+      res.succeed_or_not = false;
+    }
+  }
+  else if (req.operation == disoccupy)
+  {
+    Ocg.set_disoccupy(point);
+    res.succeed_or_not = true;
+  }
+  end_time = clock();
+
+  ROS_INFO("answered,total runtime: %f", (double)(end_time - start_time) / CLOCKS_PER_SEC);
+  return true;
 }
 
-int main()
+int main(int argc, char **argv)
 {
-  cout << round_coor(-1.11) << " " << round_coor(-4.330) << " " << round_coor(-4.8) << endl;
+  ros::init(argc, argv, "scehdule_node");
+  ros::NodeHandle nh;
+  ros::ServiceServer server = nh.advertiseService("/occupy_grid", server_callback);
+  ros::spin();
   return 0;
 }
